503a8ea2b3afaca1095d21e83664678fc0d5418a
ITSTEP\st <st>
2024-02-20 17:19:03 +0300

    .

--- Project01/Task01.cpp
+++ Project01/Task01.cpp
@@ -2,19 +2,8 @@
 
 using namespace std;
 
-#define size 100
-
-void random_init_matrix(int matrix[size][size], int max, int min,
-	int classes, int students);
-string convert_matrix_to_string(int matrix[size][size], int classes, int students);
-
 
 int main() {
-	//int max = 10, min = 0;
-
-	//int classes, students;
-
-	//int matrix[size][size];
 
 	int n = 10;
 
